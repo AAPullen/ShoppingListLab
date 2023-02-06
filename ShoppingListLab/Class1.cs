13cd73e26cc94068caa89d1603475bf9897e8d34
Andrew <andrewpullen1@gmail.com>
2023-02-06 16:48:59 -0500

    init commit

---
+++ ShoppingListLab/Class1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingListLab
+{
+    public static class ShoppingList
+    {
+        public static void MenuItems(Dictionary<string, decimal> menu)
+        { 
+            int i = 1;
+            foreach (var item in menu)
+            {
+                Console.Write($"{i}. {item.Key}: $"); 
+                foreach (var price in item.Value.ToString())
+                {
+                    Console.Write($"{price}");
+                }
+                Console.WriteLine();
+                i++;
+            }
+        }
+
+        public static bool IsOnMenu(string item, Dictionary<string, decimal> menu)
+        {
+            List<string> menuItems = GetItemsOnMenu(menu);
+
+            if (menuItems.Contains(item))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static string OrderByNumber(string userItem, Dictionary<string, decimal> menu)
+        {
+            try
+            {
+                List<string> menuItems = GetItemsOnMenu(menu);
+                int index = int.Parse(userItem) - 1;
+                userItem = menuItems[index];
+                return userItem;
+            }
+            catch (Exception)
+            {
+                return userItem;
+            }
+        }    
+
+        public static List<string> GetItemsOnMenu(Dictionary<string, decimal> menu)
+        {
+            List<string> menuItems = new List<string>(menu.Keys);
+            for (int i = 0; i < menuItems.Count; i++)
+            {
+                menuItems[i] =  menuItems[i].ToLower();
+            }
+            return menuItems;
+
+        }
+
+        public static string AddToShoppingList(string userItem)
+        {
+            string[] splitItem = userItem.Split(' ');
+            for (int i = 0; i < splitItem.Length; i++)
+            {
+                char[] letters = splitItem[i].ToCharArray();
+                letters[0] = char.ToUpper(letters[0]);
+                string capitalItem = null;
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    capitalItem = capitalItem + letters[j];
+                }
+                splitItem[i] = capitalItem;
+            }
+            
+            string userKey = string.Join(' ', splitItem);
+
+            if (userKey == "X-potion")
+            {
+                userKey = "X-Potion";
+            }
+
+            return userKey;
+        }
+
+        public static bool OrderRepeat()
+        {
+            while (true)
+            {
+                Console.Write("\nWould you like to continue shopping (y/n)? ");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y")
+                {
+                    return true;
+                }
+                else if (answer == "n") 
+                {
+                    return false;
+                }
+                else if (answer != "y" && answer != "n") 
+                {
+                    Console.WriteLine("\nI'm sorry, that is not a valid response.");
+                }
+            }
+        }
+        public static bool SeeMenu()
+        {
+            while (true)
+            {
+                Console.Write("\nWould you like to see a list of our inventory again (y/n)? ");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "y")
+                {
+                    return true;
+                }
+                else if (answer == "n")
+                {
+                    return false;
+                }
+                else if (answer != "y" && answer != "n")
+                {
+                    Console.WriteLine("\nI'm sorry, that is not a valid response.");
+                }
+            }
+        }
+    }
+}
---
+++ ShoppingListLab/Program.cs
@@ -0,0 +1,88 @@
+﻿using ShoppingListLab;
+
+Dictionary<string, decimal> apothecaryShop = new Dictionary<string, decimal>();
+apothecaryShop.Add("Potion", 1.50m);
+apothecaryShop.Add("High Potion", 5.25m);
+apothecaryShop.Add("X-Potion", 17.95m);
+apothecaryShop.Add("Ether", 4.75m);
+apothecaryShop.Add("Elixer", 0.55m);
+apothecaryShop.Add("Antidote", 2.30m);
+apothecaryShop.Add("Echo Herbs", 2.00m);
+apothecaryShop.Add("Gold Needle", 3.68m);
+apothecaryShop.Add("Remedy", 22.47m);
+apothecaryShop.Add("Phoenix Down", 20.15m);
+
+List<string> shoppingList = new List<string>();
+
+string userItem;
+bool isOnMenu = false;
+
+Console.WriteLine("Welcome to Magick City of Gariland Apothecary!\n" +
+    "Here is a complete list of our inventory:\n");
+
+ShoppingList.MenuItems(apothecaryShop);
+bool orderLoop = true;
+
+
+do
+{
+    Console.Write("\nWhat item would you like to add to your order?\n" +
+        "You may either enter the item name or item number: ");
+
+    userItem = Console.ReadLine().ToLower();
+
+
+    userItem = ShoppingList.OrderByNumber(userItem, apothecaryShop);
+    isOnMenu = ShoppingList.IsOnMenu(userItem, apothecaryShop);
+
+    if (isOnMenu == true)
+    {
+        shoppingList.Add(ShoppingList.AddToShoppingList(userItem));
+
+        orderLoop = ShoppingList.OrderRepeat();
+        
+    }
+    else
+    {
+        Console.Write("\nI'm sorry, we don't carry that item.\n");
+
+        orderLoop = ShoppingList.OrderRepeat();
+    }
+
+    if (orderLoop == true)
+    {
+        bool seeMenu = ShoppingList.SeeMenu();
+        if (seeMenu == true)
+        {
+            Console.WriteLine("\nOkay, here is a list of our inventory:\n");
+            ShoppingList.MenuItems(apothecaryShop);
+        }
+        else
+        {
+            Console.WriteLine("\nOkay.");
+        }
+    }
+
+} while (orderLoop == true);
+
+Console.WriteLine("\nAlright, so here are the items you would like to purchase:\n");
+foreach (string item in shoppingList)
+{
+    Console.WriteLine(item + " $" + apothecaryShop[item]);
+}
+
+decimal total = 0.00m;
+foreach(string item in shoppingList)
+{
+    total += apothecaryShop[item];
+}
+
+string thinking = "\n...";
+for (int i = 0; i < thinking.Length; i++)
+{
+    Console.Write(thinking[i]);
+    System.Threading.Thread.Sleep(400);
+}
+
+Console.WriteLine($"and your total is: ${total}.\n" +
+    $"Thanks for shopping with us today! Goodbye!");
